ba03d52552d8921ed2d7757459f3cd91b4a80ab1
rrhurtado <rrhurtado@uc.cl>
2017-04-27 14:50:11 -0300

    User/comments and project/comments model relations

--- db/migrate/20170427171603_create_users.rb
+++ db/migrate/20170427171603_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :password
-      t.string :email
+      t.string :email, null: false
       t.string :string
 
       t.timestamps
