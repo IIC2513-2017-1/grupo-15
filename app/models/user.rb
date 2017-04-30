6b354b1f92f30d577700e607fab80865d3d6ff7d
rrhurtado <rrhurtado@uc.cl>
2017-04-30 13:20:41 -0300

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+	validates :name, presence:true, uniqueness: true, allow_blank: false
+	validates :password, presence:true, length: { in 6..20 },
+						 confirmation: true
+	validates :email, presence: true, uniqueness: true, allow_blank: false, 
+					  format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+
 	has_many :comments
 	has_many :projects
 end
