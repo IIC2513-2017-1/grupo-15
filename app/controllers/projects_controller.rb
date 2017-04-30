48ee8f1b230de3c6ce2196d8864fb72ab83f085a
lucasmarinb <lamarin@uc.cl>
2017-04-30 20:22:41 -0300

    solving routing problems with partials

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,29 +1,26 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-  before_action :set_categories, :set_rewards, only: [:edit, :new, :show]
+  before_action :set_categories, :set_rewards, only: [:edit, :new, :show, :rewards]
 
-  # GET /projects
-  # GET /projects.json
   def index
     @projects = Project.search(params[:search])
+    #raise
   end
 
-  # GET /projects/1
-  # GET /projects/1.json
+  def rewards
+    @reward = Reward.new
+  end
+
   def show
   end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit
   end
 
-  # POST /projects
-  # POST /projects.json
   def create
     @project = Project.new(project_params)
 
@@ -38,8 +35,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
-  # PATCH/PUT /projects/1.json
   def update
     respond_to do |format|
       if @project.update(project_params)
@@ -52,8 +47,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
-  # DELETE /projects/1.json
   def destroy
     @project.destroy
     respond_to do |format|
@@ -63,17 +56,16 @@ class ProjectsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_project
       @project = Project.find(params[:id])
     end
 
     def set_rewards
-      @rewards = Project.find(params[:id]).rewards ? Project.find(params[:id]).rewards : []
+      @rewards = Reward.search(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :category_id, :user_id, :start_date, :end_date,:search)
+      params.require(:project).permit(:name, :description, :category_id, :user_id, :start_date, :end_date)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :pledges, only: [:index, :new, :create]
   resources :comments
   resources :projects
-  resources :rewards, only: [:new, :create]
+  resources :rewards, only: [:index, :new, :create]
   resources :users#, except: [:index]
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
