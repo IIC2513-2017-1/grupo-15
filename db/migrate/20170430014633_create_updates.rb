cacfb96b2c9615780551db3847f9763d36a4290f
lucasmarinb <lamarin@uc.cl>
2017-04-30 23:49:08 -0300

    not saved pages -> fixed

--- db/migrate/20170430014633_create_updates.rb
+++ db/migrate/20170430014633_create_updates.rb
@@ -1,9 +1,9 @@
 class CreateUpdates < ActiveRecord::Migration[5.0]
   def change
     create_table :updates do |t|
-      t.string :title
-      t.string :description
-      t.datetime :date
+      t.string :title, null: false
+      t.string :description, null: false
+      t.datetime :date, null: false
 
       t.timestamps
     end
