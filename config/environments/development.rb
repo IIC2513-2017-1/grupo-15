b60f28c09f65e759bd8bcba82ba4302cee04c60c
rrhurtado <rrhurtado@uc.cl>
2017-05-28 18:37:04 -0400

    merge and environment fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: ENV['SMTP_USER'],
-    password: ENV['SMTP_PASSWORD']
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD']
   }
   config.action_mailer.default_url_options = {
     host: 'localhost'
--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20170528192348) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "password"
     t.string   "email",               null: false
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
